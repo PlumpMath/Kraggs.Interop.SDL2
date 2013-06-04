7a26f4452a62f90eb3fe2f36d6200b15b97d7143
Jarle Hansen <jarle.hansen@gmail.com>
2013-06-04 23:59:03 +0200

    Added SDL_error.h

---
+++ Kraggs.Interop.SDL2/Error/SDL2ErrorCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kraggs.Interop.SDL2
+{
+    /*
+    internal enum SDL2ErrorCode
+    {
+        //SDL_ENOMEM,
+        NotEnoughMemoryError,
+        //SDL_EFREAD,
+        FileReadError,        
+        //SDL_EFWRITE,
+        FileWriteError,
+        //SDL_EFSEEK,
+        FileSeekError,
+        //SDL_UNSUPPORTED,
+        NotSupportedError,
+        //SDL_LASTERROR
+        LastError
+    }
+     * */
+}
---
+++ Kraggs.Interop.SDL2/Error/apiSDL2_error.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+// mapp to system intptr
+using SDL_Window = System.IntPtr;
+using SDL_GLContext = System.IntPtr;
+using SDL_Surface = System.IntPtr;
+
+
+namespace Kraggs.Interop.SDL2
+{
+    partial class apiSDL2
+    {
+        /// <summary>
+        /// Sets the SDL error.
+        /// </summary>
+        /// <param name="fmt"></param>
+        /// <returns>unconditionally returns -1.</returns>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_SetError", CallingConvention = SDL2_CALL)]
+        public static extern int SetError(string fmt);
+
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_GetError", CallingConvention = SDL2_CALL)]
+        private static extern IntPtr GetErrorPtr();
+        /// <summary>
+        /// Returns the error as string.
+        /// </summary>
+        /// <returns></returns>
+        [DebuggerNonUserCode()]
+        public static string GetError()
+        {
+            return Marshal.PtrToStringAnsi(GetErrorPtr());
+        }
+
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_ClearError", CallingConvention = SDL2_CALL)]
+        public static extern void ClearError();
+
+        //[DllImport(SDL2_LIBRARY, EntryPoint = "SDL_Error", CallingConvention = SDL2_CALL)]
+        //internal static extern void Error(SDL2ErrorCode code);
+
+    }
+}
