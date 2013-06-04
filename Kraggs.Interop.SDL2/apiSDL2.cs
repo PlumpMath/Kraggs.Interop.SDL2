aa111a09898c392fb86faceecd21e3de632a1f99
Jarle Hansen <jarle.hansen@gmail.com>
2013-06-04 22:46:16 +0200

    Initial commit

---
+++ Kraggs.Interop.SDL2/apiSDL2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+namespace Kraggs.Interop.SDL2
+{
+    public static partial class apiSDL2
+    {
+        internal const string SDL2_LIBRARY = "SDL2.dll";
+        internal const CallingConvention SDL2_CALL = CallingConvention.StdCall;
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Kraggs.Interop.SDL2
+===========================================
+
+A simple SDL2 Interop Library for use with OpenGL.
+
+LGPL.
