ffbffc4296693ca292ffd20abb6159c177a18ee9
Jarle Hansen <jarle.hansen@gmail.com>
2013-06-05 00:12:25 +0200

    Added SDL.h

---
+++ Kraggs.Interop.SDL2/Init/SDL2SubSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Kraggs.Interop.SDL2
+{
+    public enum SDL2SubSystem
+    {
+		Timer               = 0x00000001,
+		Audio               = 0x00000010,
+		Video               = 0x00000020,
+		Joystick            = 0x00000200,
+		Haptic              = 0x00001000,
+		GameController      = 0x00002000,
+		NoParachute         = 0x00100000,
+		//INIT_EVERYTHING = (INIT_TIMER | INIT_AUDIO | INIT_VIDEO | INIT_JOYSTICK | INIT_HAPTIC | INIT_GAMECONTROLLER | INIT_NOPARACHUTE),
+        Everything = Timer | Audio | Video | Joystick | Haptic | GameController,
+
+    }
+}
---
+++ Kraggs.Interop.SDL2/Init/apiSDL2_init.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+// mapp to system intptr
+using SDL_Window = System.IntPtr;
+using SDL_GLContext = System.IntPtr;
+using SDL_Surface = System.IntPtr;
+
+
+namespace Kraggs.Interop.SDL2
+{
+    partial class apiSDL2
+    {
+        /// <summary>
+        /// This function initializes  the subsystems specified by \c flags Unless the ::SDL_INIT_NOPARACHUTE flag is set, it will install cleanup signal handlers for some commonly ignored fatal signals (like SIGSEGV).
+        /// </summary>
+        /// <param name="flags"></param>
+        /// <returns></returns>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_Init", CallingConvention = SDL2_CALL)]
+        public static extern int Init(SDL2SubSystem flags);
+
+        /// <summary>
+        /// This function initializes specific SDL subsystems
+        /// </summary>
+        /// <param name="flags"></param>
+        /// <returns></returns>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_InitSubSystem", CallingConvention = SDL2_CALL)]
+        public static extern int InitSubSystem(SDL2SubSystem flags);
+
+        /// <summary>
+        /// This function cleans up specific SDL subsystems
+        /// </summary>
+        /// <param name="flags"></param>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_QuitSubSystem", CallingConvention = SDL2_CALL)]
+        public static extern void QuitSubSystem(SDL2SubSystem flags);
+
+        /// <summary>
+        /// This function returns a mask of the specified subsystems which have previously been initialized.
+        /// </summary>
+        /// <param name="flags"></param>
+        /// <remarks>If \c flags is 0, it returns a mask of all initialized subsystems.</remarks>
+        /// <returns></returns>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_WasInit", CallingConvention = SDL2_CALL)]
+        public static extern SDL2SubSystem WasInit(SDL2SubSystem flags);
+
+        /// <summary>
+        /// This function cleans up all initialized subsystems. You should call it upon all exit conditions.
+        /// </summary>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_Quit", CallingConvention = SDL2_CALL)]
+        public static extern void Quit();
+
+    }
+}
