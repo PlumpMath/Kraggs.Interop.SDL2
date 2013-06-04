cc855a708e7cb34b004da1be5dc4b8e98df837ab
Jarle Hansen <jarle.hansen@gmail.com>
2013-06-04 23:45:58 +0200

    Added SDL_platform.h

---
+++ Kraggs.Interop.SDL2/Platform/apiSDL2_platform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+// mapp to system intptr
+using SDL_Window = System.IntPtr;
+using SDL_GLContext = System.IntPtr;
+using SDL_Surface = System.IntPtr;
+
+namespace Kraggs.Interop.SDL2
+{
+    partial class apiSDL2
+    {
+        /// <summary>
+        /// Gets the name of the platform.
+        /// </summary>
+        /// <returns></returns>
+        [DllImport(SDL2_LIBRARY, EntryPoint = "SDL_GetPlatform", CallingConvention = SDL2_CALL)]
+        private static extern IntPtr GetPlatformPtr();
+
+        /// <summary>
+        /// Gets the name of the platform.
+        /// </summary>
+        /// <returns></returns>
+        [DebuggerNonUserCode()]
+        public static string GetPlatform()
+        {
+            return Marshal.PtrToStringAnsi(GetPlatformPtr());
+        }
+
+    }
+}
